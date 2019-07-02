0db4e50a5bcfb0cb6ff9dae45456983f1f9ac44c
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-02 22:30:32 +0100

    Modifications to searchbar // event handler callback to parent plus initial setup up env for youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { term: 'sadkasdkadka' };
+  state = { term: '' };
 
-  onInputChange = () => {
-
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
   };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Make sure we call
+    // callback from parent component
+
+  }
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
-              value={this.state.term}>
-              onChange={this.onInputChange}
+              value={this.state.term}
+              onChange={this.onInputChange}>
             </input>
           </div>
         </form>
