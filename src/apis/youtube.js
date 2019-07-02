0e260a803389a507c6e5b2a0f34e6e896cf182b7
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-02 23:22:27 +0100

    Rolled back Axios version due to bugs with youtube params. Fully implemented api code and the callback in app component

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const KEY = process.env.REACT_APP_YOUTUBE_API;
 
 export default axios.create({
-  baseURL = 'https://www.googleapis.com/youtube/v3/',
+  baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
-    part: 'snippet',
+    part: "snippet",
     maxResults: 5,
-    key: KEY,
+    key: KEY
   }
 });
