9e9016baa39d2fc065855e2b5b30f4a39ecf6720
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-03 02:33:08 +0100

    Updates

---
+++ precache-manifest.4c74bfe8818c3d3de9be04bb001f90d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a0e3586ec0f61c54788a236ec5b6d1",
+    "url": "/react-youtube-app/index.html"
+  },
+  {
+    "revision": "b336ca52de2d4e6fa13e",
+    "url": "/react-youtube-app/static/css/main.81d7c035.chunk.css"
+  },
+  {
+    "revision": "60e8b962508740592b39",
+    "url": "/react-youtube-app/static/js/2.60cf27c0.chunk.js"
+  },
+  {
+    "revision": "b336ca52de2d4e6fa13e",
+    "url": "/react-youtube-app/static/js/main.38f12558.chunk.js"
+  },
+  {
+    "revision": "44545d3ed7a6c76ce8bf",
+    "url": "/react-youtube-app/static/js/runtime~main.28f5d290.js"
+  }
+]);
