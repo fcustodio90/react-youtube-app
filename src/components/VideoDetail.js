bd665bca6c44eccd4168492eaa14f1b76a53fd41
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-03 01:52:01 +0100

    Conditional rendering for VideoDetail when app loads up and videoSelected props is null

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
-  return <div>{video.snippet.title}</div>
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+  return <div>{video.snippet.title}</div>;
 };
 
 export default VideoDetail;
